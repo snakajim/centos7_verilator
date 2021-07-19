d725c62b650353dc99a134f9e64d6203a5cf57a7
Satoshi Nakajima <satoshi.nakajima@arm.com>
2021-07-19 19:13:02 +0900

    x11 without ssh examples

---
+++ xeyes/run_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker build -t xeyes_test .
+
+docker kill xeyes
+docker system prune -f 
+
+docker run --rm -it \
+  --net host \
+  -e DISPLAY=unix${DISPLAY} \
+  -v /tmp/.X-unix:/tmp/.X-unix \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  --name xeyes \
+  xeyes_test
