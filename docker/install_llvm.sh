2d3c91aab071ac51b295e7a725ac0dbcdb32ebd6
snakajim <satoshi.nakajima@arm.com>
2022-02-11 18:23:20 +0900

    refresh some tools

--- docker/install_llvm.sh
+++ docker/install_llvm.sh
@@ -3,12 +3,12 @@
 # Intall LLVM
 # https://llvm.org/docs/GettingStarted.html#checkout
 #
-# used for CENTOS only
+# used for CENTOS7 only
 #
 LLVM_VERSION="12.0.1"
 
 mkdir -p ${HOME}/tmp && rm -rf ${HOME}/tmp/llvm-project* 
-cd ${HOME}/tmp && aria2c -x10 https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/llvm-project-${LLVM_VERSION}.src.tar.xz
+cd ${HOME}/tmp && aria2c -x10 https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-project-${LLVM_VERSION}.src.tar.xz
 cd ${HOME}/tmp && unxz llvm-project-${LLVM_VERSION}.src.tar.xz && \
   tar xvf llvm-project-${LLVM_VERSION}.src.tar && \
   cd llvm-project-${LLVM_VERSION}.src && mkdir -p build && cd build && \
@@ -16,9 +16,10 @@ cd ${HOME}/tmp && unxz llvm-project-${LLVM_VERSION}.src.tar.xz && \
     -DCMAKE_C_COMPILER="/opt/rh/devtoolset-8/root/usr/bin/gcc" \
     -DCMAKE_CXX_COMPILER="/opt/rh/devtoolset-8/root/usr/bin/g++" \
     -DLLVM_ENABLE_PROJECTS="clang;compiler-rt;lld" \
+    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi" \
     -DCMAKE_BUILD_TYPE=RELEASE \
     -DLLVM_TARGETS_TO_BUILD="ARM;X86;AArch64"\
-    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_1201" \
+    -DCMAKE_INSTALL_PREFIX="/usr/local/llvm_${LLVM_VERSION}" \
     ../llvm && make -j`nproc` && \
     sudo make install
 cd ${HOME}/tmp/llvm-project-${LLVM_VERSION}.src/build && make clean
@@ -28,7 +29,7 @@ cd ${HOME} && \
 cd ${HOME} && \
   sudo echo "# LLVM setting" >> .bashrc
 cd ${HOME} && \
-  sudo echo "export LLVM_DIR=/usr/local/llvm_1201">> .bashrc
+  sudo echo "export LLVM_DIR=/usr/local/llvm_${LLVM_VERSION}">> .bashrc
 cd ${HOME} && \
   sudo echo "export PATH=\$LLVM_DIR/bin:\$PATH" >> .bashrc
 
@@ -37,6 +38,6 @@ cd /etc/skel && \
 cd /etc/skel && \
   sudo echo "# LLVM setting" >> .bashrc
 cd /etc/skel && \
-  sudo echo "export LLVM_DIR=/usr/local/llvm_1201">> .bashrc
+  sudo echo "export LLVM_DIR=/usr/local/llvm_${LLVM_VERSION}">> .bashrc
 cd /etc/skel && \
   sudo echo "export PATH=\$LLVM_DIR/bin:\$PATH" >> .bashrc
